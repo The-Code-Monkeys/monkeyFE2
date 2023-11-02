b5698dde62438c2771df5e2a4c095d49601c88b8
Tirth-Thakkar <tirth1226@gmail.com>
2023-11-01 18:30:01 -0700

    pray hopefully

--- server/src/main/java/com/corundumstudio/socketio/demo/MotorSocketSetup.java
+++ server/src/main/java/com/corundumstudio/socketio/demo/MotorSocketSetup.java
@@ -1,47 +1,60 @@
-package com.corundumstudio.socketio.demo;
+=package com.corundumstudio.socketio.demo;
 
 import com.corundumstudio.socketio.listener.*;
-
 import java.util.HashSet;
 import java.util.Set;
-
+import java.util.concurrent.Semaphore;
 import com.corundumstudio.socketio.*;
-
 
 public class MotorSocketSetup {
 
-        public static Set<String> userQueue = new HashSet<>();
+    // A set to keep track of connected users
+    public static Set<String> userQueue = new HashSet<>();
+
+    // A semaphore to control the number of concurrent connections (1 connection at a time)
+    private static Semaphore connectionSemaphore = new Semaphore(1);
 
     public static void main(String[] args) throws InterruptedException {
 
+        // Configure the Socket.IO server
         Configuration config = new Configuration();
         config.setHostname("localhost");
         config.setPort(9092);
 
         final SocketIOServer server = new SocketIOServer(config);
 
-        // probably have to change `String.class` to `Integer.class` I'd check if ID is a integer or stinrg
-        server.addEventListener("checkUser", String.class, new DataListener<String>(){
+        // Event handler for checking user existence
+        server.addEventListener("checkUser", String.class, new DataListener<String>() {
             @Override
-            public void onData(final SocketIOClient client, String userId, final AckRequest ackRequest){
+            public void onData(final SocketIOClient client, String userId, final AckRequest ackRequest) {
+                // Check if the user exists in the set
                 boolean userExists = userQueue.contains(userId);
+                // Send the result back to the client
                 client.sendEvent("checkExist", userExists);
-                System.out.printf("User with id %s exists "+userId);
-
+                System.out.printf("User with id %s exists " + userId);
             }
         });
 
-
+        // Connect listener to handle new client connections
         server.addConnectListener(new ConnectListener() {
             @Override
             public void onConnect(SocketIOClient client) {
-                String userId = client.getSessionId().toString();
-                userQueue.add(userId);
-                server.getBroadcastOperations().sendEvent("newUserJoined", "New user joined with ID: "+userId);
-                System.out.println("New user joined with ID: " + userId);
+                // Try to acquire a connection permit
+                if (connectionSemaphore.tryAcquire()) {
+                    String userId = client.getSessionId().toString();
+                    // Add the user to the set of connected users
+                    userQueue.add(userId);
+                    // Broadcast a message to all clients
+                    server.getBroadcastOperations().sendEvent("newUserJoined", "New user joined with ID: " + userId);
+                    System.out.println("New user joined with ID: " + userId);
+                } else {
+                    // If the permit is not available, refuse the connection by disconnecting the client
+                    client.disconnect();
+                }
             }
-            
         });
+
+        // Event handlers for motor control
         server.addEventListener("up", String.class, new DataListener<String>() {
             @Override
             public void onData(final SocketIOClient client, String data, final AckRequest ackRequest) {
@@ -54,7 +67,7 @@ public class MotorSocketSetup {
         server.addEventListener("left", String.class, new DataListener<String>() {
             @Override
             public void onData(final SocketIOClient client, String data, final AckRequest ackRequest) {
-                // run code to go left
+                // Run code to go left
                 System.out.println("going left");
                 MotorFunctions.left();
             }
@@ -62,30 +75,29 @@ public class MotorSocketSetup {
 
         server.addEventListener("right", String.class, new DataListener<String>() {
             @Override
-            public void onData(final SocketIOClient client, String data, final AckRequest ackRequest) {
-                // run code to go right
+            public onData(final SocketIOClient client, String data, final AckRequest ackRequest) {
+                // Run code to go right
                 System.out.println("going right");
                 MotorFunctions.right();
             }
         });
 
-
         server.addEventListener("down", String.class, new DataListener<String>() {
             @Override
-            public void onData(final SocketIOClient client, String data, final AckRequest ackRequest) {
-                // run code to go down
+            public onData(final SocketIOClient client, String data, final AckRequest ackRequest) {
+                // Run code to go down
                 System.out.println("going down");
                 MotorFunctions.down();
             }
         });
 
-
-
+        // Start the Socket.IO server
         server.start();
 
+        // Keep the program running until interrupted
         Thread.sleep(Integer.MAX_VALUE);
 
+        // Stop the server when done
         server.stop();
     }
-
 }
