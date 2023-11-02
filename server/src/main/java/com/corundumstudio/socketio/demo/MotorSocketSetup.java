9244f40e1f85926473b5d7c76d61957c824fec7c
Tirth-Thakkar <tirth1226@gmail.com>
2023-11-02 12:22:05 -0700

    testing the frontend

--- server/src/main/java/com/corundumstudio/socketio/demo/MotorSocketSetup.java
+++ server/src/main/java/com/corundumstudio/socketio/demo/MotorSocketSetup.java
@@ -1,4 +1,5 @@
 package com.corundumstudio.socketio.demo;
+
 import com.corundumstudio.socketio.listener.*;
 import java.util.HashSet;
 import java.util.Set;
@@ -23,7 +24,7 @@ public class MotorSocketSetup {
             public void onData(final SocketIOClient client, String userId, final AckRequest ackRequest) {
                 boolean userExists = userQueue.contains(userId);
                 client.sendEvent("checkExist", userExists);
-                System.out.printf("User with id %s exists " + userId);
+                System.out.printf("User with id %s exists", userId);
             }
         });
 
@@ -41,42 +42,56 @@ public class MotorSocketSetup {
             }
         });
 
+        server.addDisconnectListener(new DisconnectListener() {
+            @Override
+            public void onDisconnect(SocketIOClient client) {
+                connectionSemaphore.release(); // Release the semaphore when a user disconnects
+                String userId = client.getSessionId().toString();
+                userQueue.remove(userId);
+                System.out.println("User disconnected with ID: " + userId);
+            }
+        });
+
         server.addEventListener("up", String.class, new DataListener<String>() {
             @Override
             public void onData(final SocketIOClient client, String data, final AckRequest ackRequest) {
                 // Run code to go up
                 System.out.println("going up");
-                // Call a motor control function to move up
+                // Call a motor control function to move up (assuming MotorFunctions is
+                // correctly implemented)
                 MotorFunctions.up();
             }
         });
 
         server.addEventListener("left", String.class, new DataListener<String>() {
             @Override
-            public void onData(final SocketIOClient client, String data, final AckRequest ackRequest) {
+            public onData(final SocketIOClient client, String data, final AckRequest ackRequest) {
                 // Run code to go left
                 System.out.println("going left");
-                // Call a motor control function to move left
+                // Call a motor control function to move left (assuming MotorFunctions is
+                // correctly implemented)
                 MotorFunctions.left();
             }
         });
 
         server.addEventListener("right", String.class, new DataListener<String>() {
             @Override
-            public void onData(final SocketIOClient client, String data, final AckRequest ackRequest) {
+            public onData(final SocketIOClient client, String data, final AckRequest ackRequest) {
                 // Run code to go right
                 System.out.println("going right");
-                // Call a motor control function to move right
+                // Call a motor control function to move right (assuming MotorFunctions is
+                // correctly implemented)
                 MotorFunctions.right();
             }
         });
 
         server.addEventListener("down", String.class, new DataListener<String>() {
             @Override
-            public void onData(final SocketIOClient client, String data, final AckRequest ackRequest) {
+            public onData(final SocketIOClient client, String data, final AckRequest ackRequest) {
                 // Run code to go down
                 System.out.println("going down");
-                // Call a motor control function to move down
+                // Call a motor control function to move down (assuming MotorFunctions is
+                // correctly implemented)
                 MotorFunctions.down();
             }
         });
@@ -87,5 +102,4 @@ public class MotorSocketSetup {
 
         server.stop();
     }
-
 }
