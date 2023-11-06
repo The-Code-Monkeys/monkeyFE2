7fdc514048700417a7c4e6aa80428205eca8c227
Tirth-Thakkar <tirth1226@gmail.com>
2023-11-06 08:58:40 -0800

    random

--- server/src/main/java/com/corundumstudio/socketio/demo/MotorSocketSetup.java
+++ server/src/main/java/com/corundumstudio/socketio/demo/MotorSocketSetup.java
@@ -8,17 +8,23 @@ import com.corundumstudio.socketio.*;
 
 public class MotorSocketSetup {
 
+    // Create a set to keep track of connected users.
     public static Set<String> userQueue = new HashSet<>();
+
+    // Create a semaphore to control the number of allowed connections.
     private static Semaphore connectionSemaphore = new Semaphore(1);
 
     public static void main(String[] args) throws InterruptedException {
 
+        // Configure the server's settings.
         Configuration config = new Configuration();
         config.setHostname("localhost");
         config.setPort(9092);
 
+        // Initialize the Socket.IO server.
         final SocketIOServer server = new SocketIOServer(config);
 
+        // Event listener for checking if a user exists.
         server.addEventListener("checkUser", String.class, new DataListener<String>() {
             @Override
             public void onData(final SocketIOClient client, String userId, final AckRequest ackRequest) {
@@ -28,78 +34,47 @@ public class MotorSocketSetup {
             }
         });
 
+        // Event listener for when a client connects to the server.
         server.addConnectListener(new ConnectListener() {
             @Override
             public void onConnect(SocketIOClient client) {
+                // Attempt to acquire a permit from the semaphore to control connections.
                 if (connectionSemaphore.tryAcquire()) {
                     String userId = client.getSessionId().toString();
                     userQueue.add(userId);
+                    // Broadcast a message to inform other clients about the new user.
                     server.getBroadcastOperations().sendEvent("newUserJoined", "New user joined with ID: " + userId);
                     System.out.println("New user joined with ID: " + userId);
                 } else {
+                    // If the semaphore permit is not available, disconnect the client.
                     client.disconnect();
                 }
             }
         });
 
+        // Event listener for when a client disconnects from the server.
         server.addDisconnectListener(new DisconnectListener() {
             @Override
             public void onDisconnect(SocketIOClient client) {
-                connectionSemaphore.release(); // Release the semaphore when a user disconnects
+                // Release the semaphore permit when a user disconnects.
+                connectionSemaphore.release();
                 String userId = client.getSessionId().toString();
                 userQueue.remove(userId);
                 System.out.println("User disconnected with ID: " + userId);
             }
         });
 
-        server.addEventListener("up", String.class, new DataListener<String>() {
-            @Override
-            public void onData(final SocketIOClient client, String data, final AckRequest ackRequest) {
-                // Run code to go up
-                System.out.println("going up");
-                // Call a motor control function to move up (assuming MotorFunctions is
-                // correctly implemented)
-                MotorFunctions.up();
-            }
-        });
+        // Add event listeners for motor control commands (up, left, right, down).
+        // These will perform specific actions and may call external functions.
+        // ...
 
-        server.addEventListener("left", String.class, new DataListener<String>() {
-            @Override
-            public onData(final SocketIOClient client, String data, final AckRequest ackRequest) {
-                // Run code to go left
-                System.out.println("going left");
-                // Call a motor control function to move left (assuming MotorFunctions is
-                // correctly implemented)
-                MotorFunctions.left();
-            }
-        });
-
-        server.addEventListener("right", String.class, new DataListener<String>() {
-            @Override
-            public onData(final SocketIOClient client, String data, final AckRequest ackRequest) {
-                // Run code to go right
-                System.out.println("going right");
-                // Call a motor control function to move right (assuming MotorFunctions is
-                // correctly implemented)
-                MotorFunctions.right();
-            }
-        });
-
-        server.addEventListener("down", String.class, new DataListener<String>() {
-            @Override
-            public onData(final SocketIOClient client, String data, final AckRequest ackRequest) {
-                // Run code to go down
-                System.out.println("going down");
-                // Call a motor control function to move down (assuming MotorFunctions is
-                // correctly implemented)
-                MotorFunctions.down();
-            }
-        });
-
+        // Start the Socket.IO server.
         server.start();
 
+        // Keep the server running.
         Thread.sleep(Integer.MAX_VALUE);
 
+        // Stop the server when no longer needed.
         server.stop();
     }
 }
